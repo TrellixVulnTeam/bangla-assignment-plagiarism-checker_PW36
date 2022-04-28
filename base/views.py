6c8da8fd03c46e42d58d8c7d025bfaec6839f412
Anis5221 <parsonal494@gmail.com>
2022-04-29 02:10:35 +0600

    some changes made

--- base/views.py
+++ base/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 from django.views.generic import View
 from .models import Document
 from difflib import SequenceMatcher 
-# Create your views here.
+
 
 class PlagView(View):
     def get(self, request):
@@ -15,6 +15,21 @@ class PlagView(View):
         if request.method == 'POST':
             form.doc1 = request.POST['doc1']
             form.doc2 = request.POST['doc2']
+            # file upload from request.FILES
+            files = request.FILES.getlist('file')
+            if files:
+                for f in files:
+                    # read the pdf file
+                    with open(f.name, 'wb+') as destination:
+                        for chunk in f.chunks():
+                            destination.write(chunk)
+                    # save the file in the database 
+                    
+                    form.doc1 = f.name
+                    
+                return HttpResponse('File uploaded.')
+            else:
+                return HttpResponse('No files uploaded.')
             form.save()
             similarity_ratio = SequenceMatcher(None,form.doc1,form.doc2).ratio()
             result = similarity_ratio * 100
