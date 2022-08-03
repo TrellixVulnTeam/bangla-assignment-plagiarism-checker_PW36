0b1e6a5522e2bf0d960e7f912e2897d7f4faf27e
anis5221 <parsonal494@gmail.com>
2022-08-04 00:37:26 +0600

    multiple file update done with ocr

--- base/views.py
+++ base/views.py
@@ -22,42 +22,37 @@ class PlagView(View):
             form.doc1 = request.POST['doc1']
             form.doc2 = request.POST['doc2']
             # file upload from request.FILES
-            pdf = request.FILES['file']
+            for pdf in request.FILES.getlist("files"):
             # return HttpResponse(files)
-            if pdf:
-                
-                out_directory = Path("~").expanduser()
-                image_file_list = []
-                text_file = out_directory / Path("out_text.txt")
-                all_text = ''
-                key = 0;
-                # return HttpResponse(pytesseract.get_languages(config=''))
-                with TemporaryDirectory() as tempdir:
-                    pdf_pages = convert_from_bytes(pdf.read())
-                    for page_enumeration, page in enumerate(pdf_pages, start=1):
-                        filename = f"{tempdir}\page_{page_enumeration:03}.jpg"
-                        page.save(filename, "JPEG")
-                        image_file_list.append(filename)
-                    else:
-                        for image_file in image_file_list:
-                            text = pytesseract.image_to_string(Image.open(image_file),lang="ben")
-                            text = text.replace("-\n", "")
-                            text = text.replace("\n", " ")
-                            text = text.replace("\t", " ")
-                            all_text += text
-                    
-                            if key == 0:
-                                key = 1
-                        
-                        # create text file
-                        # output_file.write(text)
-                tokenizeText = self.tokenize(all_text)
-                return HttpResponse(keys)
-            else:
-                return HttpResponse('No files uploaded.')
-            form.save()
-            similarity_ratio = SequenceMatcher(None,form.doc1,form.doc2).ratio()
-            result = similarity_ratio * 100
+            
+                if pdf:
+
+                    out_directory = Path("~").expanduser()
+                    image_file_list = []
+                    text_file = out_directory / Path("out_text.txt")
+                    all_text = ''
+                    key = 0;
+                    # return HttpResponse(pytesseract.get_languages(config=''))
+                    with TemporaryDirectory() as tempdir:
+                        pdf_pages = convert_from_bytes(pdf.read())
+                        for page_enumeration, page in enumerate(pdf_pages, start=1):
+                            filename = f"{tempdir}\page_{page_enumeration:03}.jpg"
+                            page.save(filename, "JPEG")
+                            image_file_list.append(filename)
+                        else:
+                            for image_file in image_file_list:
+                                text = pytesseract.image_to_string(Image.open(image_file),lang="ben")
+                                all_text += text
+                    tokenizeText = self.tokenize(all_text)
+                    # split text into sentences and tokenize each sentence coma separated 
+                    # tokenizeText = [tokenizeText[0].split(',') ]
+                    return HttpResponse(tokenizeText)
+                    return HttpResponse(tokenizeText)
+                else:
+                    return HttpResponse('No files uploaded.')
+            # form.save()
+            # similarity_ratio = SequenceMatcher(None,form.doc1,form.doc2).ratio()
+            # result = similarity_ratio * 100
            
         return render(request, 'base/master.html', {'hello':result, 'form':form})
     
